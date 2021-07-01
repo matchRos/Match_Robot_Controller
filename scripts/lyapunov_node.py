d96b644ff957c9677a2fb9738d1147e16589da4a
TobiasROSer <86289042+TobiasROSer@users.noreply.github.com>
2021-07-01 11:55:39 +0200

    improved formating

--- scripts/lyapunov_node.py
+++ scripts/lyapunov_node.py
@@ -15,15 +15,12 @@ class lyapunov_controller_node:
         rospy.init_node("lyapunov_controller_node")
         rospy.loginfo("lyapunov controller running")
         self.config()
-        self.act_pose = Odometry()
+        #self.act_pose = Odometry()
         self.target_pose = Pose()
         self.target_pose = float("Inf")
-
         self.controller_out = Twist()
 
 
-        # AMCL
-        #rospy.Subscriber("/mir1/amcl_pose", PoseWithCovarianceStamped, self.act_pose_cb)
         rospy.Subscriber(self.follower_pose_topic, Odometry, self.act_pose_cb)
         rospy.Subscriber(self.master_pose_topic, Odometry, self.target_pose_cb)
         rospy.Subscriber(self.master_vel_topic, Odometry, self.target_vel_cb)
@@ -46,11 +43,8 @@ class lyapunov_controller_node:
     def act_pose_cb(self,data):
         if self.target_pose != float("Inf"):
             act_pose = data.pose.pose
-            #print((act_pose.orientation.x,act_pose.orientation.y,act_pose.orientation.z,act_pose.orientation.w))
             act_w = tf.transformations.euler_from_quaternion((act_pose.orientation.x,act_pose.orientation.y,act_pose.orientation.z,act_pose.orientation.w))
             target_w = tf.transformations.euler_from_quaternion((self.target_pose.orientation.x,self.target_pose.orientation.y,self.target_pose.orientation.z,self.target_pose.orientation.w))
-            self.e_w = target_w[2]- act_w[2]
-            
 
             R_act = transformations.euler_matrix(act_w[0],act_w[1],-act_w[2])
             R_target = transformations.euler_matrix(target_w[0],target_w[1],target_w[2])
@@ -60,28 +54,25 @@ class lyapunov_controller_node:
             rel[1] = R_target[1,0] * self.rel_pose[0] + R_target[1,1] * self.rel_pose[1] 
 
             
-            ex = self.target_pose.position.x - act_pose.position.x - rel[0]
-            ey = self.target_pose.position.y - act_pose.position.y - rel[1]
+            ex = self.target_pose.position.x - act_pose.position.x - rel[0] # error in x (global frame)
+            ey = self.target_pose.position.y - act_pose.position.y - rel[1] # error in y (global frame)
+            e_w = target_w[2]- act_w[2]    # angular error
 
 
-            self.e_x = R_act[0,0] * ex + R_act[0,1] * ey 
-            self.e_y = R_act[1,0] * ex + R_act[1,1] * ey
-
-            print(self.e_x,self.e_y,self.e_w)
+            e_x = R_act[0,0] * ex + R_act[0,1] * ey #  error in x (local frame)
+            e_y = R_act[1,0] * ex + R_act[1,1] * ey #  error in y (local frame)
 
 
-            self.v_d = math.sqrt(self.target_vel.linear.x**2 + self.target_vel.linear.y**2)
-            #self.v_d = 0.01
-            #self.w_d = 0.1
-            self.w_d = self.target_vel.angular.z
+            v_d = math.sqrt(self.target_vel.linear.x**2 + self.target_vel.linear.y**2)
+            w_d = self.target_vel.angular.z
 
-            self.controller_out.linear.x = self.Kx * self.e_x + self.v_d * math.cos(self.e_w)
-            self.controller_out.angular.z = self.w_d + self.Ky  * self.e_y + self.Kw * math.sin(self.e_w)
+            self.controller_out.linear.x = self.Kx * e_x + v_d * math.cos(e_w)
+            self.controller_out.angular.z = w_d + self.Ky  * e_y + self.Kw * math.sin(e_w)
             self.pub.publish(self.controller_out)
 
             
         else:
-            rospy.loginfo("no target pose")
+            rospy.loginfo_throttle(2, "no target pose")
 
     def target_pose_cb(self,data):
         self.target_pose = data.pose.pose
