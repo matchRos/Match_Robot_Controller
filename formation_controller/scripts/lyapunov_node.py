f28b05a8abf65d48bdfcf84cd9cd00a898035618
TobiasROSer <86289042+TobiasROSer@users.noreply.github.com>
2021-07-05 13:08:55 +0200

    changed structure

---
+++ formation_controller/scripts/lyapunov_node.py
@@ -0,0 +1,135 @@
+#! /usr/bin/env python
+
+from operator import mod
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Twist, PoseStamped, TwistStamped
+from nav_msgs.msg import Odometry
+import tf
+import geometry_msgs
+import math
+from tf import transformations
+from rostopic import get_topic_type
+
+
+class lyapunov_controller_node:
+
+    def __init__(self):
+        rospy.init_node("lyapunov_controller_node")
+        rospy.loginfo("lyapunov controller running")
+        self.config()
+        self.target_pose = Pose()
+        self.target_pose = float("Inf")
+        self.controller_out = Twist()
+        self.e_x_i = 0
+        self.e_y_i = 0
+        self.e_w_i = 0
+
+        self.callback_selector()
+        rospy.sleep(0.1)
+        rospy.Subscriber(self.follower_pose_topic, Odometry, self.act_pose_cb)
+        
+        self.pub = rospy.Publisher(self.follower_cmd_vel_topic, Twist, queue_size=10)
+
+        rospy.spin()
+
+
+    def config(self):
+        self.KPx = rospy.get_param('~KPx')
+        self.KPy = rospy.get_param('~KPy')
+        self.KPw = rospy.get_param('~KPw')
+        self.KIw = rospy.get_param('~KIw')
+        self.KIx = rospy.get_param('~KIx')
+        self.KIx_decay = rospy.get_param('~KIx_decay')
+        self.KIw_decay = rospy.get_param('~KIw_decay')
+
+        self.master_pose_topic = rospy.get_param('~target_pose_topic')
+        self.master_vel_topic = rospy.get_param('~target_vel_topic')
+        self.follower_pose_topic = rospy.get_param('~actual_pose_topic')
+        self.follower_cmd_vel_topic = rospy.get_param('~cmd_vel_topic')
+        self.rel_pose = rospy.get_param('~rel_pose')
+                
+               
+
+
+    def act_pose_cb(self,data):
+        if self.target_pose != float("Inf"):
+            act_pose = data.pose.pose
+            act_w = tf.transformations.euler_from_quaternion((act_pose.orientation.x,act_pose.orientation.y,act_pose.orientation.z,act_pose.orientation.w))
+            target_w = tf.transformations.euler_from_quaternion((self.target_pose.orientation.x,self.target_pose.orientation.y,self.target_pose.orientation.z,self.target_pose.orientation.w))
+            R_act = transformations.euler_matrix(act_w[0],act_w[1],-act_w[2])
+            R_target = transformations.euler_matrix(target_w[0],target_w[1],target_w[2])
+
+            rel = [0,0,0]
+            rel[0] = R_target[0,0] * self.rel_pose[0] + R_target[0,1] * self.rel_pose[1] 
+            rel[1] = R_target[1,0] * self.rel_pose[0] + R_target[1,1] * self.rel_pose[1] 
+
+            
+            ex = self.target_pose.position.x - act_pose.position.x - rel[0] # error in x (global frame)
+            ey = self.target_pose.position.y - act_pose.position.y - rel[1] # error in y (global frame)
+            e_w = target_w[2]- act_w[2]    # angular error
+            e_x = R_act[0,0] * ex + R_act[0,1] * ey #  error in x (local frame)
+            e_y = R_act[1,0] * ex + R_act[1,1] * ey #  error in y (local frame)
+            self.e_x_i = (1-self.KIx_decay)*self.e_x_i + e_x
+            self.e_w_i = (1-self.KIw_decay)*self.e_w_i + e_w
+
+            v_d = math.sqrt(self.target_vel.linear.x**2 + self.target_vel.linear.y**2)
+            w_d = self.target_vel.angular.z
+            
+            
+            e_w = e_w % (2*math.pi) 
+            if(e_w>=0.5*math.pi):
+                 e_w = e_w - math.pi;
+                 v_d = -v_d;    
+            elif(e_w<= -.5*math.pi):
+                 e_w = e_w + math.pi;
+                 v_d = -v_d;            
+            
+            self.controller_out.linear.x = self.KPx * e_x + v_d * math.cos(e_w) + self.KIx * self.e_x_i
+            self.controller_out.angular.z = w_d + self.KPy * v_d * e_y + self.KPw * math.sin(e_w) + self.KIw * self.e_w_i 
+            self.pub.publish(self.controller_out)
+
+            
+        else:
+            rospy.loginfo_throttle(2, "no target pose")
+
+    
+    def callback_selector(self):
+                
+        topic_type =get_topic_type(self.master_pose_topic)
+        if topic_type[0] == "geometry_msgs/Pose":
+            rospy.Subscriber(self.master_pose_topic, Pose, self.target_pose_Pose_cb)
+        if topic_type[0] == "geometry_msgs/PoseStamped":
+                rospy.Subscriber(self.master_pose_topic, PoseStamped, self.target_pose_Pose_Stamped_cb)
+        elif topic_type[0] == "nav_msgs/Odometry":
+            rospy.Subscriber(self.master_pose_topic, Odometry, self.target_pose_cb)
+            
+        topic_type =get_topic_type(self.master_vel_topic)
+        if topic_type[0] == "geometry_msgs/Twist":
+            rospy.Subscriber(self.master_vel_topic, Twist, self.target_vel_Twist_cb)
+        if topic_type[0] == "geometry_msgs/TwistStamped":
+                rospy.Subscriber(self.master_vel_topic, TwistStamped, self.target_vel_Twist_Stamped_cb)
+        elif topic_type[0] == "nav_msgs/Odometry":
+            rospy.Subscriber(self.master_vel_topic, Odometry, self.target_vel_cb)
+            
+    
+    def target_pose_cb(self,data):
+        self.target_pose = data.pose.pose
+        
+    def target_pose_Pose_cb(self,data):
+        self.target_pose = data
+        
+    def target_pose_Pose_Stamped_cb(self,data):
+        self.target_pose = data.pose
+           
+    def target_vel_Twist_cb(self,data):
+        self.target_vel = data
+        
+    def target_vel_cb(self,data):
+        self.target_vel = data.twist.twist
+        
+    def target_vel_Twist_Stamped_cb(self,data):
+        self.target_vel = data.twist
+
+
+if __name__=="__main__":
+    lyapunov_controller_node()
---
+++ formation_controller/scripts/virtual_master.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Twist, PoseStamped, TransformStamped, Transform, TwistStamped
+from nav_msgs.msg import Odometry
+import tf
+import geometry_msgs
+import math
+from tf import transformations
+from rostopic import get_topic_type
+import tf2_ros
+
+class lyapunov_controller_node:
+
+    def __init__(self):
+        rospy.init_node("virtual_master_node")
+        rospy.loginfo("virtual_master_node running")
+        self.config()
+        self.time_old = rospy.get_time()
+        self.master_pose = PoseStamped()
+        self.master_vel = TwistStamped()
+        self.d_pose = [0,0,0]
+        self.d_pose_R = [0,0,0]
+        self.master_orientation = 0
+
+        rospy.Subscriber(self.set_pose_topic, PoseStamped, self.set_pose_cb)
+        rospy.Subscriber(self.cmd_vel_topic, TwistStamped, self.cmd_vel_cb)
+        self.pub        = rospy.Publisher(self.master_pose_topic, PoseStamped, queue_size=10)
+        self.pub_vel    = rospy.Publisher(self.master_vel_topic, TwistStamped, queue_size=10)
+        self.run()
+
+        rospy.spin()
+
+
+    def config(self):
+        self.rate = rospy.get_param('~rate')
+        self.set_pose_topic = rospy.get_param('~set_pose_topic')
+        self.master_vel_topic = rospy.get_param('~master_vel_topic')
+        self.master_pose_topic = rospy.get_param('~master_pose_topic')
+        self.cmd_vel_topic = rospy.get_param('~cmd_vel_topic')
+        
+    def run(self):
+        Rate = rospy.Rate(self.rate)
+        while not rospy.is_shutdown():
+            time_current = rospy.get_time()
+            duration = time_current - self.time_old
+            self.time_old = time_current
+            
+            self.d_pose[0] = self.master_vel.twist.linear.x * duration
+            self.d_pose[1] = self.master_vel.twist.linear.y * duration
+            self.d_pose[2] = self.master_vel.twist.angular.z * duration
+            
+            R = transformations.euler_matrix(0,0,self.master_orientation)
+            
+            self.d_pose_R[0] = R[0,0] * self.d_pose[0] + R[0,1] * self.d_pose[1] 
+            self.d_pose_R[1] = R[1,0] * self.d_pose[0] + R[1,1] * self.d_pose[1] 
+            self.d_pose_R[2] = self.d_pose[2]
+            
+            self.master_pose.pose.position.x = self.master_pose.pose.position.x + self.d_pose_R[0]
+            self.master_pose.pose.position.y = self.master_pose.pose.position.y + self.d_pose_R[1]
+            self.master_orientation = self.master_orientation + self.d_pose_R[2]
+            
+            q = transformations.quaternion_from_euler(0,0,self.master_orientation)
+            self.master_pose.pose.orientation.x = q[0]
+            self.master_pose.pose.orientation.y = q[1]
+            self.master_pose.pose.orientation.z = q[2]
+            self.master_pose.pose.orientation.w = q[3]
+            
+            self.master_pose.header.stamp = rospy.Time.now()
+            self.master_vel.header.stamp = rospy.Time.now()
+            
+            self.pub.publish(self.master_pose)
+            self.pub_vel.publish(self.master_vel)
+            
+            
+            br = tf2_ros.TransformBroadcaster()
+            t = TransformStamped()
+            t.header.stamp = rospy.Time.now()
+            t.header.frame_id = "map"
+            t.child_frame_id = "virtual_master/base_footprint"
+            t.transform.translation = self.master_pose.pose.position
+            t.transform.rotation = self.master_pose.pose.orientation
+            br.sendTransform(t)
+            
+            Rate.sleep()
+
+    def set_pose_cb(self,data):
+        self.master_pose = data
+        orientation = transformations.euler_from_quaternion([data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w])
+        self.master_orientation = orientation[2]
+        
+
+    def cmd_vel_cb(self,data):
+        self.master_vel = data
+       
+
+
+if __name__=="__main__":
+    lyapunov_controller_node()
