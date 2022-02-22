1c346eb5b9646eea692e368673410ab2734ebc55
matchRos <Recker@match.uni-hannover.de>
2022-02-22 14:23:48 +0100

    finished execute trajectories for any number of robot

--- formation_controller/scripts/execute_trajectories_dynamic.py
+++ formation_controller/scripts/execute_trajectories_dynamic.py
@@ -6,9 +6,6 @@ from mypath import MyTrajectory
 from nav_msgs.msg import Path
 import math
 from geometry_msgs.msg import Twist, PoseWithCovarianceStamped, Pose
-from nav_msgs.msg import Odometry
-from tf import transformations
-from l_phi_controller import l_phi_controller
 from cartesian_controller import cartesian_controller
 
 
@@ -37,13 +34,12 @@ class execute_trajectories_node():
         while not rospy.is_shutdown() and idx < len(self.target_trajectories[0].v):
          
             for i in range(0,self.number_of_robots):
-                print(self.target_trajectories[i].x[idx])
                 act_pose        = self.robot_poses[i]
                 set_pose_x      = self.target_trajectories[i].x[idx]
                 set_pose_y      = self.target_trajectories[i].y[idx]
-                set_pose_phi      = self.target_trajectories[i].phi[idx]
-                w_target        = self.target_trajectories[i].w[idx]
-                v_target        = self.target_trajectories[i].v[idx]
+                set_pose_phi    = self.target_trajectories[i].phi[idx]
+                w_target        = self.target_trajectories[i].w[idx] * self.control_rate
+                v_target        = self.target_trajectories[i].v[idx] * self.control_rate
                 u_v, u_w = cartesian_controller(act_pose,set_pose_x,set_pose_y,w_target,v_target,set_pose_phi)
 
                 self.robot_command.linear.x = u_v
@@ -51,12 +47,7 @@ class execute_trajectories_node():
                 self.cmd_vel_publishers[i].publish(self.robot_command)
 
                 self.target_pose_broadcaster([set_pose_x,set_pose_y,set_pose_phi],i)
-            
-            # self.target_pose_broadcaster([self.robot0_trajectory.x[idx],self.robot0_trajectory.y[idx],self.robot0_trajectory.phi[idx]], \
-            #     [self.robot1_trajectory.x[idx],self.robot1_trajectory.y[idx],self.robot1_trajectory.phi[idx]], \
-            #     [self.robot2_trajectory.x[idx],self.robot2_trajectory.y[idx],self.robot2_trajectory.phi[idx]])
-
-            # self.actual_pose_broadcaster(self.robot0_act_pose,self.robot1_act_pose,self.robot2_act_pose)
+                self.actual_pose_broadcaster(act_pose,i)
 
             idx += 1
             rate.sleep()
@@ -75,7 +66,6 @@ class execute_trajectories_node():
             phi = math.atan2(Path.poses[i+1].pose.position.y-Path.poses[i].pose.position.y,Path.poses[i+1].pose.position.x-Path.poses[i].pose.position.x)
             trajectory.phi.append(phi)
         
-
         trajectory.v = [0.0]
         trajectory.w = [0.0]
         for i in range(1,path_len-2):
@@ -83,7 +73,7 @@ class execute_trajectories_node():
             trajectory.w.append(trajectory.phi[i+1]-trajectory.phi[i])
 
         self.target_trajectories.append(trajectory)
-        print("trajecory " + str(robot_index) +" received")
+        rospy.loginfo("trajecory " + str(robot_index) +" received")
 
 
     def robot_pose_cb(self,Pose,robot_index):
@@ -96,22 +86,11 @@ class execute_trajectories_node():
                      tf.transformations.quaternion_from_euler(0, 0, target_pose[2]),
                      rospy.Time.now(), frame_id, "map")
 
-
-    def actual_pose_broadcaster(self,robot0_act_pose,robot1_act_pose,robot2_act_pose):
-        self.robot0_actual_pose_broadcaster.sendTransform((robot0_act_pose.position.x,robot0_act_pose.position.y,0), 
-        (robot0_act_pose.orientation.x,robot0_act_pose.orientation.y,robot0_act_pose.orientation.z,robot0_act_pose.orientation.w),
-        rospy.Time.now(), "robot0/actual_pose", "map")
-
-        self.robot1_actual_pose_broadcaster.sendTransform((robot1_act_pose.position.x,robot1_act_pose.position.y,0), 
-        (robot1_act_pose.orientation.x,robot1_act_pose.orientation.y,robot1_act_pose.orientation.z,robot1_act_pose.orientation.w),
-        rospy.Time.now(), "robot1/actual_pose", "map")
-
-        self.robot2_actual_pose_broadcaster.sendTransform((robot2_act_pose.position.x,robot2_act_pose.position.y,0), 
-        (robot2_act_pose.orientation.x,robot2_act_pose.orientation.y,robot2_act_pose.orientation.z,robot2_act_pose.orientation.w),
-        rospy.Time.now(), "robot2/actual_pose", "map")
-       
-
-
+    def actual_pose_broadcaster(self,actual_pose,robot_id):
+            frame_id = "robot" + str(robot_id) + "/actual_pose"
+            self.pose_broadcaster.sendTransform((actual_pose.position.x, actual_pose.position.y, 0),
+                     (actual_pose.orientation.x,actual_pose.orientation.y,actual_pose.orientation.z,actual_pose.orientation.w),
+                     rospy.Time.now(), frame_id, "map")
 
     def config(self):
         self.number_of_robots = rospy.get_param("~number_of_robots")
@@ -120,6 +99,7 @@ class execute_trajectories_node():
         self.robot_poses = []
         self.cmd_vel_publishers = []
         self.robot_command = Twist()
+        self.pose_broadcaster = tf.TransformBroadcaster()
 
         for i in range(0,self.number_of_robots):
             param = "~robot" + str(i) + "_trajectory_topic"
@@ -135,37 +115,8 @@ class execute_trajectories_node():
             topic = rospy.get_param(param)
             self.cmd_vel_publishers.append(rospy.Publisher(topic,Twist,queue_size=5))
 
-        self.robot0_trajectory = MyTrajectory()
-        self.robot1_trajectory = MyTrajectory()
-        self.robot2_trajectory = MyTrajectory()
-        # robot0_cmd_vel_topic = rospy.get_param("~robot0_cmd_vel_topic")
-        # robot1_cmd_vel_topic = rospy.get_param("~robot1_cmd_vel_topic")
-        # robot2_cmd_vel_topic = rospy.get_param("~robot2_cmd_vel_topic")
-        # self.robot0_twist_publisher = rospy.Publisher(robot0_cmd_vel_topic,Twist,queue_size=5)
-        # self.robot1_twist_publisher = rospy.Publisher(robot1_cmd_vel_topic,Twist,queue_size=5)
-        # self.robot2_twist_publisher = rospy.Publisher(robot2_cmd_vel_topic,Twist,queue_size=5)
-        # self.robot0_twist = Twist()
-        # self.robot1_twist = Twist()
-        # self.robot2_twist = Twist()
-        self.robot0_trajectory_received = False
-        self.robot1_trajectory_received = False
-        self.robot2_trajectory_received = False
-        self.robot0_act_pose = Pose()
-        self.robot1_act_pose = Pose()
-        self.robot2_act_pose = Pose()
-        # robot0_pose_topic = rospy.get_param("~robot0_pose_topic")
-        # robot1_pose_topic = rospy.get_param("~robot1_pose_topic")
-        # robot2_pose_topic = rospy.get_param("~robot2_pose_topic")
-        # rospy.Subscriber(robot0_pose_topic, Pose, self.robot0_pose_cb)
-        # rospy.Subscriber(robot1_pose_topic, Pose, self.robot1_pose_cb)
-        # rospy.Subscriber(robot2_pose_topic, Pose, self.robot2_pose_cb)
-        self.pose_broadcaster = tf.TransformBroadcaster()
-        self.robot0_target_pose_broadcaster = tf.TransformBroadcaster()
-        self.robot1_target_pose_broadcaster = tf.TransformBroadcaster()
-        self.robot2_target_pose_broadcaster = tf.TransformBroadcaster()
-        self.robot0_actual_pose_broadcaster = tf.TransformBroadcaster()
-        self.robot1_actual_pose_broadcaster = tf.TransformBroadcaster()
-        self.robot2_actual_pose_broadcaster = tf.TransformBroadcaster()
+       
+
 
 
 if __name__=="__main__":
