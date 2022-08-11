6563f9b345c02fee622278be3c1952242e2df94e
matchRos <Recker@match.uni-hannover.de>
2022-08-11 13:28:56 +0200

    added multi stage process to move to initial pose

--- formation_controller/scripts/cartesian_controller.py
+++ formation_controller/scripts/cartesian_controller.py
@@ -7,17 +7,19 @@ from geometry_msgs.msg import Pose, Twist
 
 
 def cartesian_controller(actual_pose = Pose(),target_pose = Pose(),target_velocity = Twist()):
-    Kv = 0.5
-    Ky = 0.45
-    Kx = 0.3
+    Kv = 0.0
+    Ky = 0.1
+    Kx = 0.0
     phi_act = transformations.euler_from_quaternion([actual_pose.orientation.x,actual_pose.orientation.y,actual_pose.orientation.z,actual_pose.orientation.w])
     phi_target = transformations.euler_from_quaternion([target_pose.orientation.x,target_pose.orientation.y,target_pose.orientation.z,target_pose.orientation.w])
 
     e_x = (target_pose.position.x-actual_pose.position.x)
     e_y = (target_pose.position.x - actual_pose.position.y)
-    rospy.loginfo_throttle(1,[id,e_x,e_y])
+    #rospy.loginfo_throttle(1,[id,e_x,e_y])
     e_local_x = math.cos(phi_act[2]) * e_x + math.sin(phi_act[2]) * e_y
     e_local_y = math.cos(phi_act[2]) * e_y - math.sin(phi_act[2]) * e_x
+
+    #print(target_velocity)
 
     u_w = target_velocity.angular.z + target_velocity.linear.x * Kv * e_local_y + Ky * math.sin(phi_target[2]-phi_act[2])
     u_v = target_velocity.linear.x * math.cos(phi_target[2]-phi_act[2]) + Kx*e_local_x
--- formation_controller/scripts/execute_trajectories_dynamic.py
+++ formation_controller/scripts/execute_trajectories_dynamic.py
@@ -4,7 +4,7 @@ import rospy
 import tf
 from tf import transformations
 from mypath import MyTrajectory
-from nav_msgs.msg import Path
+from nav_msgs.msg import Path, Odometry
 import math
 from geometry_msgs.msg import Twist, PoseWithCovarianceStamped, Pose
 from cartesian_controller import cartesian_controller
@@ -32,46 +32,48 @@ class execute_trajectories_node():
 
         # Move to initial pose
         # Turn towards the initial pose
-        correct_orientation = 0
-        while not rospy.is_shutdown() and (correct_orientation < self.number_of_robots):
-            for i in range(0,self.number_of_robots):
-                act_pose        = self.robot_poses[i]
-                set_pose_x      = self.target_trajectories[i].x[0]
-                set_pose_y      = self.target_trajectories[i].y[0]
-                phi_actual = transformations.euler_from_quaternion([act_pose.orientation.x,act_pose.orientation.y,act_pose.orientation.z,act_pose.orientation.w])
-                phi_target = math.atan2(set_pose_y-act_pose.position.y,set_pose_x-act_pose.position.x)
-                e_phi = phi_target - phi_actual[2]
-
-                self.robot_command.angular.z = self.K_phi * e_phi
-                if abs(self.robot_command.angular.z) > self.limit_w:
-                    self.robot_command.angular.z = self.robot_command.angular.z / abs(self.robot_command.angular.z) * self.limit_w
-                if abs(e_phi) < self.target_threshhold_angular:
-                    self.robot_command.angular.z = 0
-                    correct_orientation += 1
-                self.cmd_vel_publishers[i].publish(self.robot_command)
-
-                print(i,"phi",e_phi,self.robot_command.angular.z)
-            rate.sleep()
-
-        # move linear to the initial pose
-        correct_distance = 0
-        while not rospy.is_shutdown() and correct_distance < self.number_of_robots:
+        for i in range(0,self.initial_pose_iterations):
+            correct_orientation = 0
+            while not rospy.is_shutdown() and (correct_orientation < self.number_of_robots):
                 for i in range(0,self.number_of_robots):
                     act_pose        = self.robot_poses[i]
                     set_pose_x      = self.target_trajectories[i].x[0]
                     set_pose_y      = self.target_trajectories[i].y[0]
-                    e_d = math.sqrt((set_pose_x-act_pose.position.x)**2 + (set_pose_y-act_pose.position.y) **2 )
+                    phi_actual = transformations.euler_from_quaternion([act_pose.orientation.x,act_pose.orientation.y,act_pose.orientation.z,act_pose.orientation.w])
+                    phi_target = math.atan2(set_pose_y-act_pose.position.y,set_pose_x-act_pose.position.x)
+                    e_phi = phi_target - phi_actual[2]
 
-                    self.robot_command.linear.x = self.K_d * e_d
-                    if abs(self.robot_command.linear.x) > self.limit_x:
-                        self.robot_command.linear.x = self.robot_command.linear.x / abs(self.robot_command.linear.x) * self.limit_x
-                    if abs(e_d) < self.target_threshhold_linear:
-                        self.robot_command.linear.x = 0
-                        correct_distance += 1
+                    self.robot_command.angular.z = self.K_phi * e_phi
+                    if abs(self.robot_command.angular.z) > self.limit_w:
+                        self.robot_command.angular.z = self.robot_command.angular.z / abs(self.robot_command.angular.z) * self.limit_w
+                    if abs(e_phi) < self.target_threshhold_angular:
+                        self.robot_command.angular.z = 0
+                        correct_orientation += 1
                     self.cmd_vel_publishers[i].publish(self.robot_command)
-                
-                    print("l",e_d)
-                    rate.sleep()
+
+                    print(i,"phi",e_phi,self.robot_command.angular.z)
+                rate.sleep()
+
+            # move linear to the initial pose
+            correct_distance = 0
+            while not rospy.is_shutdown() and correct_distance < self.number_of_robots:
+                    for i in range(0,self.number_of_robots):
+                        act_pose        = self.robot_poses[i]
+                        set_pose_x      = self.target_trajectories[i].x[0]
+                        set_pose_y      = self.target_trajectories[i].y[0]
+                        e_d = math.sqrt((set_pose_x-act_pose.position.x)**2 + (set_pose_y-act_pose.position.y) **2 )
+
+                        self.robot_command.linear.x = self.K_d * e_d
+                        if abs(self.robot_command.linear.x) > self.limit_x:
+                            self.robot_command.linear.x = self.robot_command.linear.x / abs(self.robot_command.linear.x) * self.limit_x
+                        if abs(e_d) < self.target_threshhold_linear:
+                            self.robot_command.linear.x = 0
+                            correct_distance += 1
+                        self.cmd_vel_publishers[i].publish(self.robot_command)
+                    
+                        print("l",e_d)
+                        rate.sleep()
+            self.target_threshhold_linear *= 0.5
 
         # set correct orientation
         correct_orientation = 0
@@ -92,24 +94,36 @@ class execute_trajectories_node():
                 
                     print(e_phi)
             
+            
               
         while not rospy.is_shutdown() and idx < len(self.target_trajectories[0].v):
          
             for i in range(0,self.number_of_robots):
-                act_pose        = self.robot_poses[i]
-                set_pose_x      = self.target_trajectories[i].x[idx]
-                set_pose_y      = self.target_trajectories[i].y[idx]
-                set_pose_phi    = self.target_trajectories[i].phi[idx]
-                w_target        = self.target_trajectories[i].w[idx] * self.control_rate
-                v_target        = self.target_trajectories[i].v[idx] * self.control_rate
-                u_v, u_w = cartesian_controller(act_pose,set_pose_x,set_pose_y,w_target,v_target,set_pose_phi)
+                actual_pose     = self.robot_poses[i]
+
+                target_pose     = Pose()
+                target_pose.position.x = self.target_trajectories[i].x[idx]
+                target_pose.position.y = self.target_trajectories[i].y[idx]
+                q = transformations.quaternion_from_euler(0,0,self.target_trajectories[i].phi[idx])
+                target_pose.orientation.x = q[0]
+                target_pose.orientation.y = q[1]
+                target_pose.orientation.z = q[2]
+                target_pose.orientation.w = q[3]
+
+                target_velocity = Twist()
+                target_velocity.linear.x = self.target_trajectories[i].v[idx] * self.control_rate
+                target_velocity.angular.z = self.target_trajectories[i].w[idx] * self.control_rate
+
+                u_v, u_w = cartesian_controller(actual_pose,target_pose,target_velocity)
+                rospy.loginfo_throttle(1, "u_v: %f, u_w: %f", u_v, u_w)
+                rospy.loginfo_throttle(1, "target velocity= " + str(target_velocity.linear.x))
 
                 self.robot_command.linear.x = u_v
                 self.robot_command.angular.z = u_w
                 self.cmd_vel_publishers[i].publish(self.robot_command)
 
-                self.target_pose_broadcaster([set_pose_x,set_pose_y,set_pose_phi],i)
-                self.actual_pose_broadcaster(act_pose,i)
+                self.target_pose_broadcaster(target_pose,i)
+                # self.actual_pose_broadcaster(actual_pose,i)
 
             idx += 1
             rate.sleep()
@@ -140,12 +154,13 @@ class execute_trajectories_node():
 
     def robot_pose_cb(self,Pose,robot_index):
         self.robot_poses[robot_index] = Pose.pose.pose
+        self.actual_pose_broadcaster(Pose.pose.pose,robot_index)
 
 
     def target_pose_broadcaster(self,target_pose,robot_id):
         frame_id = "robot" + str(robot_id) + "/target_pose"
-        self.pose_broadcaster.sendTransform((target_pose[0], target_pose[1], 0),
-                     tf.transformations.quaternion_from_euler(0, 0, target_pose[2]),
+        self.pose_broadcaster.sendTransform((target_pose.position.x, target_pose.position.y, 0),
+                     (target_pose.orientation.x, target_pose.orientation.y, target_pose.orientation.z, target_pose.orientation.w),
                      rospy.Time.now(), frame_id, "map")
 
     def actual_pose_broadcaster(self,actual_pose,robot_id):
@@ -166,8 +181,9 @@ class execute_trajectories_node():
         self.K_d = 0.5
         self.limit_w = 0.5
         self.limit_x = 0.2
-        self.target_threshhold_angular = 0.03
-        self.target_threshhold_linear = 0.15
+        self.target_threshhold_angular = 0.02
+        self.target_threshhold_linear = 1.0
+        self.initial_pose_iterations = 3
 
         for i in range(0,self.number_of_robots):
             param = "~robot" + str(i) + "_trajectory_topic"
@@ -177,7 +193,7 @@ class execute_trajectories_node():
             param = "~robot" + str(i) + "_pose_topic"
             robotX_pose_topic = rospy.get_param(param)
             self.robot_poses.append(Pose())
-            rospy.Subscriber(robotX_pose_topic, PoseWithCovarianceStamped, self.robot_pose_cb, i)
+            rospy.Subscriber(robotX_pose_topic, Odometry, self.robot_pose_cb, i) #PoseWithCovarianceStamped
 
             param = "~robot" + str(i) + "_cmd_vel_topic"
             topic = rospy.get_param(param)
